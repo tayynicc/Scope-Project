735df847d4a77369c30e89008be35a0da6f81a86
Austin Williams <austinwill264@gmail.com>
2021-03-18 13:56:16 -0700

    completed 02

--- scope-problems-without-context-starter/problems/02-arrow-my-map.js
+++ scope-problems-without-context-starter/problems/02-arrow-my-map.js
@@ -1,7 +1,7 @@
 /***********************************************************************
 Write a function using fat arrow syntax named `arrowMyMap` that accepts an array
 and a callback as arguments. The function will return an array of new elements
-obtained by calling the callback on each element of the array, passing in the 
+obtained by calling the callback on each element of the array, passing in the
 element. Assign the below function to a variable using the const keyword.
 
 Do not use the built in Array#map - use Array#forEach for iteration.
@@ -17,18 +17,18 @@ let result2 = arrowMyMap(['run', 'Forrest'], yell);
 console.log(result2);   // [ 'RUN!', 'FORREST!' ]
 
 ***********************************************************************/
-//understand 
-  //using fat arrow syntax to pass in array elements throught a callback 
+//understand
+  //using fat arrow syntax to pass in array elements throught a callback
   //returning new array with mod elements without return statement
 //plan
-  // declare variable with given name with const 
+  // declare variable with given name with const
   // create a array and cd param
-  // create empty array 
-  // for loop 
+  // create empty array
+  // for loop
   // create a variable for current el
   // conditional if
   //passing current el in cb
-  //pushing el form cb to new array 
+  //pushing el form cb to new array
 //do
 
 const arrowMyMap = (arr, cb) => {
@@ -36,15 +36,16 @@ const arrowMyMap = (arr, cb) => {
   for(let i = 0; i < arr.length; i++){
     let currentEl = arr[i];
     newArr.push(cb(currentEl));
-    
-    
-    //if(cb(currentEl)){
-    //  newArr.push(currentEl);
-    
   }
+  return newArr
 }
-let result1 = arrowMyMap([100, 25, 81, 64], Math.sqrt);
-console.log(result1);   // [ 10, 5, 9, 8 ]
+
+console.log(arrowMyMap([100, 25, 81, 64], Math.sqrt));   // [ 10, 5, 9, 8 ]
+
+const yell = el => el.toUpperCase() + '!'
+
+let result2 = arrowMyMap(['run', 'Forrest'], yell);
+console.log(result2);   // [ 'RUN!', 'FORREST!' ]
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
--- scope-problems-without-context-starter/problems/03-arrow-rest-sum.js
+++ scope-problems-without-context-starter/problems/03-arrow-rest-sum.js
@@ -1,6 +1,5 @@
 /***********************************************************************
-Write a function named `arrowRestSum` that accepts all incoming parameters
-and sums them. Assign the below function to a variable using the const keyword.
+
 
 **Hint**: Use rest parameter syntax!
 
@@ -8,10 +7,15 @@ Examples:
 arrowRestSum(3,5,6); // => 14
 arrowRestSum(1, 2, 3, 4, 5, 6, 7, 8, 9); // => 14
 arrowRestSum(0); // => 0
+
+Write a function named `arrowRestSum` that accepts all incoming parameters
+and sums them. Assign the below function to a variable using the const keyword.
 ***********************************************************************/
 
 // your code here!
-
+arrowRestSum(3,5,6); // => 14
+arrowRestSum(1, 2, 3, 4, 5, 6, 7, 8, 9); // => 14
+arrowRestSum(0); // => 0
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
