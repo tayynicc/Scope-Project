0f6879c724eb8d7b548930f8ced9186fa0dfae85
Taylor <tw10262@gmail.com>
2021-03-18 15:30:36 -0700

    solve problem 5

--- scope-problems-without-context-starter/problems/05-arrow-mirror-array.js
+++ scope-problems-without-context-starter/problems/05-arrow-mirror-array.js
@@ -9,9 +9,31 @@ Examples:
 arrowMirrorArray([1,2,3]); // => [ 1, 2, 3, 3, 2, 1 ]
 arrowMirrorArray(['a', 'b', 'c', 'd']); // => [ 'a', 'b', 'c', 'd', 'd', 'c', 'b', 'a' ]
 ***********************************************************************/
+//understand 
+  //taking in arr and outputting new arr with the reversed array added on.  
+//plan
+  // declare function using arrow syntax with arr param
+  
+  // declare an empty array (result)
+  //create a variable for result arr 
+  // use forEach() to loop
+  // add current el to new arr
+  //reverse orig arr and then push to new arr 
 
-// your code here
+//do
 
+let arrowMirrorArray = (arr) =>{
+  let result = [];
+  for (let i = 0; i < arr.length; i++){
+    result.push(arr[i]);
+  }
+  for (let j = arr.length - 1; j >= 0; j--){
+    result.push(arr[j]);
+  }
+  return result;
+}
+
+console.log(arrowMirrorArray([1,2,3]));
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
