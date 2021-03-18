c2f89de55fb64be1c90146e9d46e43995aa870b4
Taylor <tw10262@gmail.com>
2021-03-18 14:30:26 -0700

    solved problem 03

--- scope-problems-without-context-starter/problems/03-arrow-rest-sum.js
+++ scope-problems-without-context-starter/problems/03-arrow-rest-sum.js
@@ -11,11 +11,40 @@ arrowRestSum(0); // => 0
 Write a function named `arrowRestSum` that accepts all incoming parameters
 and sums them. Assign the below function to a variable using the const keyword.
 ***********************************************************************/
+// understand
+  //creating arrow function that take in all param ... and sums them 
+  // assing function to const
+//plan
+  //set const variable 
+  //create a rest param
+  //create a counter variable
+  //counter += param variable 
+  //return counter 
+//do
+  //
 
-// your code here!
-arrowRestSum(3,5,6); // => 14
-arrowRestSum(1, 2, 3, 4, 5, 6, 7, 8, 9); // => 14
-arrowRestSum(0); // => 0
+// const  arrowRestSum = (...num) => {
+//   let newArr = [];
+//   newArr.push(num)
+//   console.log(newArr + "---");
+  
+//   //let counter = 0; 
+//   //let result = counter += num; 
+
+//   //return result; 
+// }
+
+const arrowRestSum = (...nums) => {
+  let counter = 0; 
+  for(let num of nums){
+    counter += num;
+  }
+  return counter;
+}
+
+console.log(arrowRestSum(3,5,6)); // => 14
+console.log(arrowRestSum(1, 2, 3, 4, 5, 6, 7, 8, 9)); // => 14
+console.log(arrowRestSum(0)); // => 0
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
