e78d27263a19184d1b6ee726ac755ffe89422378
Austin Williams <austinwill264@gmail.com>
2021-03-18 16:12:33 -0700

    incompleted 06

--- scope-problems-without-context-starter/problems/06-closure-sandwiches.js
+++ scope-problems-without-context-starter/problems/06-closure-sandwiches.js
@@ -1,6 +1,6 @@
 /***********************************************************************
 Let's practice writing closures by creating a function named `sandwichMaker()`.
-This function will return another function that will accept a string to add 
+This function will return another function that will accept a string to add
 to the sandwich order (which will start off with a default "tomato" ingredient),
 separating each incoming ingredient with "and".
 
@@ -14,10 +14,41 @@ sandwich("bread") // => "One sandwich with tomato and spinach and jelly and brea
 Another Sandwich:
 let sandwich2 = sandwichMaker(); // => returns a function
 sandwich2("pb") // => "One sandwich with tomato and pb"
-
+Let's practice writing closures by creating a function named `sandwichMaker()`.
+This function will return another function that will accept a string to add
+to the sandwich order (which will start off with a default "tomato" ingredient),
+separating each incoming ingredient with "and".
 ***********************************************************************/
+//UNDERSTAND
+//function will return another function
+//second function accepts a string
+//returns a string, uses a string of and
+//PLAN
+//set up function using arrow syntax
+//one function will be anonymous
+//second func will be a cb func
+//create cb func
+//inside cb func we will concat with string
+//nice
 
-// your code here!
+let sandwichMaker = (ingre) => {
+
+function sandwich(ingre) {
+let str = 'One sandwich with tomato and '
+return str + ingre
+}
+
+}
+
+let sandwich = sandwichMaker(); // => returns a function
+console.log(sandwich)
+sandwich("spinach") // => "One sandwich with tomato and spinach"
+sandwich("jelly") // => "One sandwich with tomato and spinach and jelly"
+sandwich("bread") // => "One sandwich with tomato and spinach and jelly and bread"
+
+let sandwich2 = sandwichMaker(); // => returns a function
+sandwich2("pb") // => "One sandwich with tomato and pb"
+console.log(sandwich2)
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
