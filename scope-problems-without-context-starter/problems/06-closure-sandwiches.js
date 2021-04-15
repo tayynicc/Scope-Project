02ee4f534c1afd660670493899c0e3fdf020812c
Austin Williams <austinwill264@gmail.com>
2021-04-15 12:50:33 -0700

    01

--- scope-problems-without-context-starter/problems/06-closure-sandwiches.js
+++ scope-problems-without-context-starter/problems/06-closure-sandwiches.js
@@ -19,36 +19,29 @@ This function will return another function that will accept a string to add
 to the sandwich order (which will start off with a default "tomato" ingredient),
 separating each incoming ingredient with "and".
 ***********************************************************************/
-//UNDERSTAND
-//function will return another function
-//second function accepts a string
-//returns a string, uses a string of and
-//PLAN
-//set up function using arrow syntax
-//one function will be anonymous
-//second func will be a cb func
-//create cb func
-//inside cb func we will concat with string
-//nice
 
-let sandwichMaker = (ingre) => {
 
-function sandwich(ingre) {
-let str = 'One sandwich with tomato and '
-return str + ingre
+
+function sandwichMaker() {
+  let sandwichIngredients = []
+
+return function(ingredient) {
+let finalResponse = 'One sandwich with tomato'
+}
+for(let i = 0; i < sandwichIngredients; i++) {
+let currentIngredient = sandwichIngredients[i]
+sandwichIngredients.push(currentIngredient)
+}
+return finalResponse
 }
 
-}
 
 let sandwich = sandwichMaker(); // => returns a function
 console.log(sandwich)
 sandwich("spinach") // => "One sandwich with tomato and spinach"
 sandwich("jelly") // => "One sandwich with tomato and spinach and jelly"
-sandwich("bread") // => "One sandwich with tomato and spinach and jelly and bread"
 
-let sandwich2 = sandwichMaker(); // => returns a function
-sandwich2("pb") // => "One sandwich with tomato and pb"
-console.log(sandwich2)
+
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
